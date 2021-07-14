20f99eee866777e187454d5be0d967a70ebd8298
Abdul Uddin <auddin431@gmail.com>
2021-07-14 13:47:06 -0400

    cleaned some files and changed the color

--- flask-admin-boilerplate/views/__init__.py
+++ flask-admin-boilerplate/views/__init__.py
@@ -1,6 +1,5 @@
 from flask import render_template, request, redirect, url_for, session
 from app import app
-from model import *
 
 @app.route('/', methods=["GET"])
 def home():
@@ -11,47 +10,47 @@ def home():
     #     return render_template('login.html')
 
 # Register new user
-@app.route('/register', methods=["GET", "POST"])
-def register():
-    if request.method == "GET":
-        return render_template("register.html")
-    elif request.method == "POST":
-        registerUser()
-        return redirect(url_for("login"))
+# @app.route('/register', methods=["GET", "POST"])
+# def register():
+#     if request.method == "GET":
+#         return render_template("register.html")
+#     elif request.method == "POST":
+#         registerUser()
+#         return redirect(url_for("login"))
 
 #Check if email already exists in the registratiion page
-@app.route('/checkusername', methods=["POST"])
-def check():
-    return checkusername()
+# @app.route('/checkusername', methods=["POST"])
+# def check():
+#     return checkusername()
 
 # Everything Login (Routes to renderpage, check if username exist and also verifypassword through Jquery AJAX request)
-@app.route('/login', methods=["GET"])
-def login():
-    if request.method == "GET":
-        if "username" not in session:
-            return render_template("login.html")
-        else:
-            return redirect(url_for("home"))
+# @app.route('/login', methods=["GET"])
+# def login():
+#     if request.method == "GET":
+#         if "username" not in session:
+#             return render_template("login.html")
+#         else:
+#             return redirect(url_for("home"))
 
 
-@app.route('/checkloginusername', methods=["POST"])
-def checkUserlogin():
-    return checkloginusername()
+# @app.route('/checkloginusername', methods=["POST"])
+# def checkUserlogin():
+#     return checkloginusername()
 
-@app.route('/checkloginpassword', methods=["POST"])
-def checkUserpassword():
-    return checkloginpassword()
+# @app.route('/checkloginpassword', methods=["POST"])
+# def checkUserpassword():
+#     return checkloginpassword()
 
 #The admin logout
-@app.route('/logout', methods=["GET"])  # URL for logout
-def logout():  # logout function
-    session.pop('username', None)  # remove user session
-    return redirect(url_for("home"))  # redirect to home page with message
+# @app.route('/logout', methods=["GET"])  # URL for logout
+# def logout():  # logout function
+#     session.pop('username', None)  # remove user session
+#     return redirect(url_for("home"))  # redirect to home page with message
 
 #Forgot Password
-@app.route('/forgot-password', methods=["GET"])
-def forgotpassword():
-    return render_template('forgot-password.html')
+# @app.route('/forgot-password', methods=["GET"])
+# def forgotpassword():
+#     return render_template('forgot-password.html')
 
 #404 Page
 @app.route('/404', methods=["GET"])
